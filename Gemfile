2aadcac705f6209c8fb5d0ebe5f0ca24d9d0bc5e
Jay Lawrence <jay@patientway.com>
2013-10-20 11:37:17 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in jquery-keypad-rails.gemspec
+gemspec
---
+++ lib/jquery/keypad/rails.rb
@@ -0,0 +1,9 @@
+require "jquery/keypad/rails/version"
+
+module Jquery
+  module Keypad
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
